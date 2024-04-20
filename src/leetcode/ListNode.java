822ca7c174b1b71e53997bb84d3006b0c65ff4d4
sumityadavcs22 <sumit.yadav_cs22@gla.ac.in>
2024-04-20 23:29:07 +0530

    Node class created for the leetcode problem 21

---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
